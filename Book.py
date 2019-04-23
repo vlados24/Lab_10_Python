f78241b8f080b4bc5519c13e1acaed1f74a07497
Your Name <andrushkevych2424@gmail.com>
2019-04-23 10:22:51 +0300

    done lab 10

--- Book.py
+++ Book.py
@@ -1,8 +1,11 @@
+from builtins import staticmethod
+
+
 class Book:
     amount_of_books = 0
 
-    def __init__(self, name="Rainy day Smoke Bell", price_in_hryvnias=260, number_of_pages=334, author_of_the_book="Jack London",
-                 date_of_first_sale=25, color="black", genre_book= "Adventure"):
+    def __init__(self, name="", price_in_hryvnias=0, number_of_pages=0, author_of_the_book="",
+                 date_of_first_sale=0, color="", genre_book=""):
         self.name = name
         self.price_in_hryvnias = price_in_hryvnias
         self.number_of_pages = number_of_pages
@@ -10,13 +13,22 @@ class Book:
         self.date_of_first_sale = date_of_first_sale
         self.color = color
         self.genre_book = genre_book
+        Book.amount_of_books += 1
 
-    def __del__(self):
-        print("Destructor " + self.name + " deleted")
+        def __del__(self):
+            print("Destructor was called")
 
-    def __repr__(self):
-        return str(self.__dict__)
+    def __str__(self):
+        return "Book{" \
+               + " name = '" + str(self.name) + "'" \
+               + ", price_in_hryvnias = " + str(self.price_in_hryvnias) \
+               + ", number_of_pages = " + str(self.number_of_pages) \
+               + ", author_of_the_book = " + str(self.author_of_the_book) \
+               + ", date_of_first_sale = " + str(self.date_of_first_sale) \
+               + ", color = " + str(self.color) \
+               + ", genre_book = " + str(self.genre_book)
 
 
-def output_static_field():
-    return Book.amount_of_books
+    @staticmethod
+    def get_static_amount_of_books(self):
+        return Book.amount_of_books
--- Main.py
+++ Main.py
@@ -4,11 +4,11 @@ def main():
     paper_book = Book("Mountains of World", 125, 300, "Jeremy Lanister", 43, "White", "Scientific")
     magazine  = Book ("Top Gear", 300, 230, "Jeremy Clarkson")
     picture_book = Book("Photos of Galaxy")
+
     print(paper_book)
     print(magazine)
     print(picture_book)
     print(Book.amount_of_books)
 
 
-if __name__ == '__main__':
-    main()
+main()
